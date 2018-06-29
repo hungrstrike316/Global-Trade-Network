e019d58f8e18e0df601d97fe1790b8c59ad116f2
ParhamP <ppourdavood@gmail.com>
2018-06-29 05:50:43 -0700

    Add best_partition to the plots.

--- code/SVD_KMeans2.py
+++ code/SVD_KMeans2.py
@@ -6,10 +6,9 @@ import sklearn.cluster as skc
 import utils.data_manipulation as dm
 import utils.network_manipulation as nm
 import qualities as cq
-from matplotlib.font_manager import FontProperties
 
 
-class Kmeans:
+class Clustering:
     dirPre = dm.set_dir_tree()
     dirIn = str(dirPre + 'adjacency_ntwrk_npz_files/')
     countriesLL = dm.load_country_lat_lon_csv(
@@ -32,16 +31,23 @@ class Kmeans:
                              axis=1) == self.exports), 'Exports are Weird'
         self.trade_ntwrk = nm.construct_ntwrk_method(self.trade_ntwrk_graph,
                                                      method)
-        self.kmLabels = None
+        self.labels = None
 
     def svd(self):
+        """Compute Singular Value Decomposition on trade_ntwrkA
+        (without anything on the diagonal)
+
+        Returns:
+            tuple
+
+        """
         Ui, Si, Vi = np.linalg.svd(self.trade_ntwrk, full_matrices=True,
                                    compute_uv=True)
         return Ui, Si, Vi
 
     def kmeans(self, numClust, nDims,  Vi):
-        """Compute Singular Value Decomposition on trade_ntwrkA
-        (without anything on the diagonal)
+        """Computes kmeans clustering based on svd
+
         Returns:
             tuple
         """
@@ -50,8 +56,16 @@ class Kmeans:
         kmLabels = km.labels_
         kmCenters = km.cluster_centers_
         kmParams = km
-        self.kmLabels = kmLabels
+        self.labels = kmLabels
         return kmLabels, kmCenters, kmParams
+
+    def best_partition(self):
+        best_partitions = c.best_partition(self.G)
+        formatted_label = np.empty((len(best_partitions)), dtype=np.int32)
+        for country_index in best_partitions:
+            formatted_label[country_index] = best_partitions[country_index]
+        self.labels = list(formatted_label)
+        return list(formatted_label)
 
     def reformat_kmLabels_nx(self, numClust):
         """reformat kmLabels to be used in nx quality function
@@ -59,12 +73,12 @@ class Kmeans:
         Returns:
             list: list of sets. Each set contains the index of countries
         """
-        assert self.kmLabels is not None, "Run kmeans method first " \
-                                          "to get the labels."
+        assert self.labels is not None, "Run kmeans method first " \
+                                        "to get the labels."
         community = [set() for _ in range(numClust)]
-        for i in range(len(self.kmLabels)):
+        for i in range(len(self.labels)):
             # Pass the clusters without any nodes
-            community[self.kmLabels[i]].add(i)
+            community[self.labels[i]].add(i)
         return community
 
     def reformat_kmLabels_c(self):
@@ -75,23 +89,23 @@ class Kmeans:
             and values the clusters
 
         """
-        assert self.kmLabels is not None, "Run kmeans method first to get" \
-                                          "the labels."
+        assert self.labels is not None, "Run kmeans method first to get" \
+                                        "the labels."
         partition = {}
-        for i in range(len(self.kmLabels)):
-            partition[i] = self.kmLabels[i]
+        for i in range(len(self.labels)):
+            partition[i] = self.labels[i]
         return partition
 
-    def kmeans_quality_measure(self, quality_measure, labels):
+    def cluster_quality_measure(self, quality_measure, labels):
         """
 
         Returns:
             float: the quality measure of the clustering
 
         """
-        assert self.kmLabels is not None, "Run kmeans method before running" \
-                                          "quality measures in order to set" \
-                                          "the labels"
+        assert self.labels is not None, "Run kmeans method before running" \
+                                        "quality measures in order to set" \
+                                        "the labels"
         if quality_measure is 'louvain_modularity':
             assert self.flg_sym is 'sym', "louvain modularity does not accept" \
                                           "asymmetrical graphs"
@@ -113,15 +127,17 @@ class Kmeans:
                 return cq.density(self.G, labels)
             if quality_measure is "conductance":
                 return cq.conductance(self.G, labels)
+            else:
+                raise ValueError("Quality measure is not found.")
 
 
 # -----------------------------------------------------------------------------
 
-def get_labels(kmeans_object, quality_measure, numClust):
+def get_labels(cluster_object, quality_measure, numClust):
     """
 
     Args:
-        kmeans_object (Kmeans): Kmeans object
+        cluster_object (Clustering): Clustering object
         quality_measure (str): "modularity" or "density" or "conductance"
         numClust (int): number of clusters
 
@@ -129,9 +145,9 @@ def get_labels(kmeans_object, quality_measure, numClust):
 
     """
     if quality_measure is 'louvain_modularity':
-        return kmeans_object.reformat_kmLabels_c()
+        return cluster_object.reformat_kmLabels_c()
     else:
-        return kmeans_object.reformat_kmLabels_nx(numClust)
+        return cluster_object.reformat_kmLabels_nx(numClust)
 
 
 def kmeans_quality_matrix(year, method, quality_measure, numClustList,
@@ -151,20 +167,21 @@ def kmeans_quality_matrix(year, method, quality_measure, numClustList,
 
     """
     quality_gather = np.zeros((len(numClustList), len(nDimsList)))
-    kmeans_object = Kmeans(year, method, flg_sym)
-    Vi = kmeans_object.svd()[2]
+    cluster_object = Clustering(year, method, flg_sym)
+    Vi = cluster_object.svd()[2]
     for ki, k in enumerate(numClustList):
         for di, d in enumerate(nDimsList):
-            kmeans_object.kmeans(k, d, Vi)
-            labels = get_labels(kmeans_object, quality_measure, k)
-            quality = kmeans_object.kmeans_quality_measure(quality_measure,
-                                                           labels)
+            cluster_object.kmeans(k, d, Vi)
+            labels = get_labels(cluster_object, quality_measure, k)
+            quality = cluster_object.cluster_quality_measure(quality_measure,
+                                                            labels)
             quality_gather[ki][di] = quality
     return quality_gather
 
 
 def kmeans_multiple_quality_matrix(year, method, qualities_list,
-                                   numClustList, nDimsList, flg_sym):
+                                   numClustList, nDimsList, flg_sym,
+                                   best_partition=False):
     """
 
     Args:
@@ -175,23 +192,33 @@ def kmeans_multiple_quality_matrix(year, method, qualities_list,
         numClustList (list): e.g [2, 3, 5, 7, 10, 15]
         nDimsList (list): e.g [3, 5, 10, 20, 35, 50, 100, 150, 200]
         flg_sym (bool): True or False
+        best_partition (bool): Set to True if best_partition is desired.
 
     Returns:
-
+        list
     """
     quality_gathers = [np.zeros((len(numClustList), len(nDimsList))) for _ in
                        range(len(qualities_list))]
-    kmeans_object = Kmeans(year, method, flg_sym)
-    Vi = kmeans_object.svd()[2]
+    cluster_object = Clustering(year, method, flg_sym)
+    Vi = cluster_object.svd()[2]
     for quality_index in range(len(qualities_list)):
+        quality_measure = qualities_list[quality_index]
         for ki, k in enumerate(numClustList):
             for di, d in enumerate(nDimsList):
-                kmeans_object.kmeans(k, d, Vi)
-                quality_measure = qualities_list[quality_index]
-                labels = get_labels(kmeans_object, quality_measure, k)
-                quality = kmeans_object.kmeans_quality_measure(quality_measure,
-                                                               labels)
+                cluster_object.kmeans(k, d, Vi)
+                labels = get_labels(cluster_object, quality_measure, k)
+                quality = cluster_object.cluster_quality_measure(quality_measure,
+                                                                labels)
                 quality_gathers[quality_index][ki][di] = quality
+        if best_partition:
+            cluster_object.best_partition()
+            k = max(cluster_object.labels) + 1
+            labels = get_labels(cluster_object, quality_measure, k)
+            quality = cluster_object.cluster_quality_measure(quality_measure,
+                                                             labels)
+            best_partition_row = np.full((1, len(nDimsList)), quality)
+            quality_gathers[quality_index] = np.concatenate((quality_gathers[quality_index],
+                                              best_partition_row))
     return quality_gathers
 
 
@@ -202,7 +229,7 @@ def quality_plot(quality_matrix, quality, numClustList, nDimList, year, name):
         quality_matrix (numpy.ndarray): Matrix of shape
                                         len(numClustList) X len(nDimList)
         quality (str): "modularity" or "density" or "conductance"
-        numClustList (list): e.g [2, 3, 5, 7, 10, 15]
+        numClustList (list): e.g [2, 3, 5, 7, 10, 15, "best_partition]
         nDimList (list): e.g [3, 5, 10, 20, 35, 50, 100, 150, 200]
         year (int): e.g 2006
         name: name of the image. For example, "density_2006.png"
@@ -233,4 +260,3 @@ def multiple_qualities_plot(quality_matrices, qualities_list, numClustList,
         quality_plot(quality_matrices[quality_index],
                      qualities_list[quality_index], numClustList, nDimList,
                      year, name + "_" + qualities_list[quality_index] + ".png")
-    
